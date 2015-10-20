df2b4b03fdb6a0df83e680042fcd91e2c06de8a6
Gabriel Alonso <formmo@live.com>
2015-10-20 13:06:45 -0400

    Projeto inicial

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public Vector2 velocityBall = new Vector2 (0, -0);
+	public float _acceleration = 0.5f;
+	public float maxcima;
+	public float maxbaixo;
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Verifica se o personagem esta subindo ou descendo
+		if (Input.GetMouseButton (0)) {
+			GetComponent<Rigidbody2D> ().velocity = -velocityBall * _acceleration;
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = velocityBall * _acceleration/2;
+		}
+
+		//Reseta a aceleracao quando o personagem troca a direcao
+		if (Input.GetMouseButtonDown (0) || Input.GetMouseButtonUp (0)) {
+			_acceleration = 1f;
+		}
+
+		//Determina um limite para o valor da aceleracao
+		if (_acceleration > 10) {
+			_acceleration = 10;
+		}
+
+		//Aumenta o valor da aceleracao em 0.1 a cada frame
+		_acceleration = _acceleration + 0.1f;
+
+		//Mantem o personagem dentro da tela
+		if (transform.position.y >= maxcima) {			
+			transform.position = new Vector2 (transform.position.x, maxcima);
+		}
+
+		if (transform.position.y <= maxbaixo) {			
+			transform.position = new Vector2 (transform.position.x, maxbaixo);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+	public GameObject obstacle;
+	public float _time;
+	//private Score _score;
+	// Use this for initialization
+
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 3f, _time);
+		//_score = GameObject.FindGameObjectWithTag ("score").GetComponent<Score> ();
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(obstacle);
+		//_score.UpScore ();
+	}
+}
