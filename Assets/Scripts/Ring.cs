f28ec8faaf674e7d6662a066f7d51e87ec2d222f
Gabriel Alonso <formmo@live.com>
2015-10-29 09:45:13 -0400

    Changes

--- Assets/Scripts/Ring.cs
+++ Assets/Scripts/Ring.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Ring : MonoBehaviour {
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -15,8 +15,8 @@ public class Ring : MonoBehaviour {
 		if (other.gameObject.tag.Equals ("Barreira")) {
 			Application.LoadLevel (Application.loadedLevel);
 		}
-		if (other.gameObject.tag.Equals ("Ring")) {
-			// incrementa um score
+		if (other.gameObject.tag.Equals ("Star")) {
+			Destroy(other.gameObject);
 		}
 	}
 
